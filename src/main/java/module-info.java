0022e958b422a3274ebef0f4b130569177a84650
Likhith R <likhithrangam10@gmail.com>
2024-04-17 21:11:12 +0530

    AUCTION PAGE

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.iplauctionapplication {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires java.sql;
 
 
     opens org.example.iplauctionapplication to javafx.fxml;
