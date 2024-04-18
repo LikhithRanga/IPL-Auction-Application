ef39f3e2ef31c1270e96641909fc89bab3367770
Likhith R <likhithrangam10@gmail.com>
2024-04-19 00:18:12 +0530

    AUCTION PAGE

--- src/main/java/org/example/iplauctionapplication/playerModel.java
+++ src/main/java/org/example/iplauctionapplication/playerModel.java
@@ -1,6 +1,10 @@
 package org.example.iplauctionapplication;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class playerModel {
     private String playerName;
     private int playerAge;
@@ -13,5 +17,19 @@ public class playerModel {
         this.playerAge = playerAge;
         this.playerRole = playerRole;
         this.basePrice = basePrice;
+    }
+    public  void registerPlayer(String playerName,int playerAge, String playerRole, long basePrice){
+        try(Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3606/IPLAuction","root","1234")){
+            String sqlquery="INSERT INTO players(playerName,playerAge,playerRole,basePrice,teamName) VALUES(playerName,playerAge,playerRole,basePrice,NULL)";
+            try (PreparedStatement statement = connection.prepareStatement(sqlquery)) {
+                statement.setString(1, playerName);
+                statement.setInt(2, playerAge);
+                statement.setString(3, playerRole);
+                statement.setLong(3, basePrice);
+                statement.executeUpdate();
+            }
+    } catch (Exception e) {
+        e.printStackTrace();
+    }
     }
 }
