a1ed2e0a12d69317f216036853b331813b19230f
KeshavaKeka <106547733+KeshavaKeka@users.noreply.github.com>
2024-04-24 13:42:22 +0530

    Controller classes added for player and team registrations

--- src/main/java/org/example/iplauctionapplication/teamViewController.java
+++ src/main/java/org/example/iplauctionapplication/teamViewController.java
@@ -2,22 +2,39 @@ package org.example.iplauctionapplication;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 
 public class teamViewController {
     @FXML
-    private VBox buttonHolder;
+    private AnchorPane buttonHolder;
+
+
 
     public void initialize() {
-        for(int i = 1; i <= 5; i++) {
-            Button button = new Button("Button " + i);
+        double gap = 0;
+        for(int i = 1; i <= 4; i++) {
+            Label Teamname = new Label("Team"+i);
+            Button buttonBid = new Button("Bid");
+            Button buttonIgnore = new Button("Ignore Player");
 
-            button.setMinSize(30, 10);
-            button.setMaxSize(Double.MAX_VALUE, Double.MAX_VALUE);
+            //button.setScaleX();
+            buttonBid.setMinSize(100, 30);
+            buttonBid.setMaxSize(100, 35);
+            buttonIgnore.setMinSize(100, 30);
+            buttonIgnore.setMaxSize(100, 35);
 
-            buttonHolder.setSpacing(10);
+            buttonHolder.getChildren().add(Teamname);
+            buttonHolder.getChildren().add(buttonBid);
+            buttonHolder.getChildren().add(buttonIgnore);
 
-            buttonHolder.getChildren().add(button);
+            AnchorPane.setTopAnchor(Teamname,425.0);
+            AnchorPane.setLeftAnchor(Teamname, 100.0 + gap);
+            AnchorPane.setTopAnchor(buttonBid, 450.0);
+            AnchorPane.setLeftAnchor(buttonBid, 100.0 + gap);
+            AnchorPane.setTopAnchor(buttonIgnore, 500.0);
+            AnchorPane.setLeftAnchor(buttonIgnore, 100.0 + gap);
+            gap+=120;
         }
     }
 }
